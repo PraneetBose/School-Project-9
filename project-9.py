c0ec907e0e62c04e559aa18f12757384dd35c8ef
Praneet Bose <94593299+PraneetBose@users.noreply.github.com>
2023-04-11 23:03:44 +0530

    Update project-9.py

--- project-9.py
+++ project-9.py
@@ -1,8 +1,8 @@
 # Python program to check leap year or not
 year = int(input("Enter any year: "))
-if year % 4 == 0:
+if year % 4 == 0: # equal to operator i.e TRUE
     print("leap year")
 
-elif year % 4 != 0:
+elif year % 4 != 0: #not equal to operator i.e FALSE
     print("not a Leap year")
 
